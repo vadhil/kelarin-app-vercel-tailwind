5d3af8d99655b8987d5994e6fd6204fd9de96878
fadhil <88018547+vadhil@users.noreply.github.com>
2023-05-08 14:22:53 +0800

    vercel is easy

--- app/profile.jsx
+++ app/profile.jsx
@@ -1,6 +1,9 @@
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <div>
+      <h1>profile</h1>
+      <p>vercel is fun and good, u just have to make it as next project and connect it with github, then every change will be updated automatically</p>
+    </div>
   )
 }
