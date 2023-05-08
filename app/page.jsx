e8ee45fd58dc15c843177fa16a0ae383a8762433
fadhil <88018547+vadhil@users.noreply.github.com>
2023-05-08 14:03:30 +0800

    profile

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Hero from './hero'
+import Profile from './profile'
 
 export default function Home() {
   return (
       <main>
         <Hero />
+        <Profile />
         hello
       </main>
   )
