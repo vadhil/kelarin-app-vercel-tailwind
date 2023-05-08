ab758949ccb355921d8f4a6a714558967b77db60
fadhil <88018547+vadhil@users.noreply.github.com>
2023-05-08 13:45:27 +0800

    hero and nav

---
+++ app/components/navbar.jsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <header>
+        <nav className='flex gap-4  text-xl justify-around p-7 bg-prime text-white  '>
+            <Link className='font-bold' href="/">AEX Digital</Link>
+            <Link href={"/"}>home</Link>
+            <Link href={"/about"}>about</Link>
+        </nav>
+    </header>
+  )
+}
---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import Hero from './hero'
+
+export default function Home() {
+  return (
+      <main>
+        <Hero />
+      </main>
+  )
+}
